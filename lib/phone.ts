253867f2dd5cceb4b975fb1c7e1e1767063a6931
siny <siny@sinyui-MacBookAir.local>
2026-05-30 10:11:49 +0900

    fix: update default phone number

--- lib/phone.ts
+++ lib/phone.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_PHONE_NUMBER = "031-000-0000";
+export const DEFAULT_PHONE_NUMBER = "031-8039-7701";
 export const DEFAULT_PHONE_HREF = `tel:${DEFAULT_PHONE_NUMBER}`;
 
 export function toPhoneHref(phone: string | null | undefined) {
